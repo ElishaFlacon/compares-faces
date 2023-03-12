1ede0090adc506df0a12f61cbef45efb0d86d333
Elisha Flacon <elishaflacon@gmail.com>
2023-03-12 20:58:54 +0500

    minimal functionals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 
 function App() {
 
 
-    // jopa slona
+    const [valueAnswer, setValueAnswer] = useState('...');
+
+    const [valueFirstPicture, setValueFirstPicture] = useState('');
+    const [valueSecondPicture, setValueSecondPicture] = useState('');
+
+    const [getFirstPicture, setGetFirstPicture] = useState('');
+    const [getSecondPicture, setGetSecondPicture] = useState('');
+
+
+
+    const onConfirm = async () => {
+        const formData = new FormData();
+        formData.append('first_pic', valueFirstPicture[0]);
+        formData.append('second_pic', valueSecondPicture[0]);
+        const a = await axios.post('http://localhost:5000/api/post/face-verify', formData);
+        console.log(a.data[0])
+        setValueAnswer(String(a.data[0]));
+    }
 
 
     return (
         <div className="App">
-            
+            <div className='form'>
+                <div>{valueAnswer}</div>
+                <input type="file" onChange={e => setValueFirstPicture(e.target.files)} />
+                <input type="file" onChange={e => setValueSecondPicture(e.target.files)} />
+                <button onClick={onConfirm}>send</button>
+            </div>
         </div>
     );
 }
