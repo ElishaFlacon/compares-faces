17c20db4dab51dbd4be788887deb8f33355e9d80
Elisha Flacon <elishaflacon@gmail.com>
2023-03-24 22:41:07 +0500

    upd gui

--- src/App.js
+++ src/App.js
@@ -6,15 +6,37 @@ import './App.css';
 function App() {
 
 
+    const defaultImg = 'https://cdn-icons-png.flaticon.com/512/3973/3973425.png';
+    const loadImg = 'https://cdn-icons-png.flaticon.com/512/4146/4146794.png';
+    const compareImg = 'https://cdn-icons-png.flaticon.com/512/5261/5261936.png';
+
     const [valueAnswer, setValueAnswer] = useState('...');
 
     const [valueFirstPicture, setValueFirstPicture] = useState('');
     const [valueSecondPicture, setValueSecondPicture] = useState('');
 
-    const [getFirstPicture, setGetFirstPicture] = useState('');
-    const [getSecondPicture, setGetSecondPicture] = useState('');
+    const [getFirstPicture, setGetFirstPicture] = useState(defaultImg);
+    const [getSecondPicture, setGetSecondPicture] = useState(defaultImg);
 
+    const imageHandler = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                setGetFirstPicture(reader.result);
+            }
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
 
+    const imageHandler2 = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                setGetSecondPicture(reader.result);
+            }
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
 
     const onConfirm = async () => {
         const formData = new FormData();
@@ -28,12 +50,41 @@ function App() {
 
     return (
         <div className="App">
-            <div className='form'>
-                <div>{valueAnswer}</div>
-                <input type="file" onChange={e => setValueFirstPicture(e.target.files)} />
-                <input type="file" onChange={e => setValueSecondPicture(e.target.files)} />
-                <button onClick={onConfirm}>send</button>
+            <div className='pre-img-container'>
+                <div className='pre-img-box'>
+                    <img src={getFirstPicture} alt="" className='pre-img' />
+
+                    <label class="input-file">
+                        <input type="file" name="file" onChange={e => { setValueSecondPicture(e.target.files); imageHandler(e) }} />
+                        <div class="input-file-btn">
+                            <img className='load-img' src={loadImg} alt="" />
+                        </div>
+                    </label>
+                </div>
+
+
+                <div className='answer'>{valueAnswer}</div>
+
+
+                <div className='pre-img-box'>
+                    <img src={getSecondPicture} alt="" className='pre-img' />
+
+                    <label class="input-file">
+                        <input type="file" name="file" onChange={e => { setValueFirstPicture(e.target.files); imageHandler2(e) }} />
+                        <div class="input-file-btn">
+                            <img className='load-img' src={loadImg} alt="" />
+                        </div>
+                    </label>
+                </div>
             </div>
+
+
+            <button onClick={onConfirm}>
+                <h1>
+                    сравнить
+                </h1>
+                <img className='compare-img' src={compareImg} alt="send" />
+            </button>
         </div>
     );
 }
