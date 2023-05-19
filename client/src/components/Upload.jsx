49769c4e2faf3ce73d2f8f3a757a5e25045b11fb
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-19 21:54:17 +0500

    update upload component for destruction app.js

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,53 +1,94 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Button, Box, TextField } from '@mui/material';
-import ChoiseFilePicture from './ChoiseFilePicture';
+import axios from 'axios';
 import '../styles/Upload.css';
+import PictureLoader from './PictureLoader';
 
 
 function Upload(props) {
-    const uploadPictureButtonText = props.picture ? props.picture[0].name : 'Выберите фото';
-    const pictureIsLoad = props.picture ? true : false;
-    const dataIsLoad = pictureIsLoad && props.person.name && props.person.age && props.person.gender && props.person.description ? true : false;
 
+    const [person, setPerson] = useState({
+        name: '',
+        age: '',
+        gender: '',
+        description: '',
+    });
 
+    const dataIsLoad = useMemo(() => {
+        return !Object.values(person).some(element => !element) && props.picture[0];
+    }, [person, props.picture])
+
+    const changePerson = (value) => {
+        setPerson({ ...person, ...value })
+    }
+
+    const uploadPerson = async () => {
+        const formData = new FormData();
+
+        formData.append('picture', props.picture[0]);
+        for (const [key, value] of Object.entries(person)) {
+            formData.append(key, value);
+        }
+
+        clear();
+        const response = await axios.post('http://localhost:5000/api/post/upload-person', formData);
+
+        if (!response.data.load) {
+            alert('Данные не были загружены! Возможно программа не может распознать лицо на фото!');
+            return;
+        }
+        alert('Данные были загружены!');
+
+        return response.data;
+    }
+
+    const clear = () => {
+        props.setPicture()
+        setPerson({
+            name: '',
+            age: '',
+            gender: '',
+            description: '',
+        });
+    }
+
+    
     return (
         <Box className={'ub-row ub-al-end gap'}>
-            <Box className='flex-center-column'>
-                <ChoiseFilePicture file={pictureIsLoad} />
 
-                <Button variant="outlined" component="label" >
-                    {uploadPictureButtonText}
-                    <input hidden type="file" accept="image/*" onChange={props.pictureHandler} />
-                </Button>
-            </Box>
+            <PictureLoader
+                picture={props.picture}
+                setPicture={props.setPicture}
+                onChange={() => changePerson({ picture: props.picture })}
+            />
 
             <Box className="ub-column ub-jc-end gap">
                 <Box className={'ub-row gap'}>
                     <Box className={'ub-column gap'}>
                         <TextField
-                            value={props.person.name}
-                            onChange={(event) => props.changeHandler({ ...props.person, name: event.target.value })}
+                            value={person.name}
+                            onChange={(event) => changePerson({ name: event.target.value })}
                             label="ФИО"
                             variant="outlined"
                         />
                         <TextField
-                            value={props.person.age}
-                            onChange={(event) => props.changeHandler({ ...props.person, age: event.target.value })}
+                            value={person.age}
+                            onChange={(event) => changePerson({ age: event.target.value })}
                             label="Возраст"
                             variant="outlined"
                             type='number'
                         />
                         <TextField
-                            value={props.person.gender}
-                            onChange={(event) => props.changeHandler({ ...props.person, gender: event.target.value })}
+                            value={person.gender}
+                            onChange={(event) => changePerson({ gender: event.target.value })}
                             label="Пол"
                             variant="outlined"
                         />
                     </Box>
 
                     <TextField
-                        value={props.person.description}
-                        onChange={(event) => props.changeHandler({ ...props.person, description: event.target.value })}
+                        value={person.description}
+                        onChange={(event) => changePerson({ description: event.target.value })}
                         label="Описание"
                         variant="outlined"
                         multiline
@@ -56,7 +97,7 @@ function Upload(props) {
                     />
                 </Box>
 
-                <Button variant="outlined" component='button' disabled={!dataIsLoad} onClick={props.uploadHandler}>
+                <Button variant="outlined" component='button' disabled={!dataIsLoad} onClick={uploadPerson}>
                     Загрузить
                 </Button>
             </Box>
