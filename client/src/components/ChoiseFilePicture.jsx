48bce39293ffa11e549b8d967f05cbfb6744b95a
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-19 21:55:37 +0500

    rename props

--- client/src/components/ChoiseFilePicture.jsx
+++ client/src/components/ChoiseFilePicture.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function ChoiseFilePicture(props) {
     return (
-        <div className={props.file ? 'chosen-file' : 'choise-file'} />
+        <div className={props.isLoad ? 'chosen-file' : 'choise-file'} />
     );
 }
 
