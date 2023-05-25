3b5539859ddd8103e0a9616827f572ed5ce02d7a
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-26 00:41:56 +0500

    add routes

---
+++ client/src/router/index.js
@@ -0,0 +1,8 @@
+import Find from "../pages/Find";
+import Upload from "../pages/Upload";
+
+
+export const routes = [
+    { path: '/find', component: Find },
+    { path: '/upload', component: Upload },
+]
