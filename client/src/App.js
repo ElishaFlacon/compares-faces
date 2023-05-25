516c5cdd5705e10bdbf502e22b8bfa1d476a5814
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-26 00:42:27 +0500

    decomposition app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,19 @@
-import React, { useMemo, useState } from 'react';
-import { Container, Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
 import './App.css';
-import DosyLogo from './components/DosyLogo';
-import Find from './components/Find';
-import Upload from './components/Upload';
-import ModalForResults from './components/ModalForResults';
+import DosyLogo from './components/UI/logo/DosyLogo';
 import Text from './components/Text';
 import Menu from './components/Menu';
-import Snack from './components/Snack';
+import Snack from './components/UI/snackbar/Snack';
 import config from './config';
+import AppRouter from './components/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContext } from './context';
 
 
 function App() {
-    // для загрузки фоток
-    const [valuePicture, setValuePicture] = useState('');
+    const [picture, setPicture] = useState('');
 
-    // для получения данных о найденых людях с сервера
-    const [valuePersons, setValuePersons] = useState('');
-    const getPersons = (data) => {
-        setValuePersons(data);
-    }
-
-    // для модального окна с результатами поиска
-    const [modalState, setModalState] = useState(false);
-    const modalHasOpen = () => {
-        setModalState(true);
-    }
-    const modalHasClose = () => {
-        setValuePicture('');
-        setValuePersons('');
-        setModalState(false);
-    }
-
-    // для снекбара
     const [snack, setSnack] = useState(false);
     const snackClose = (event, reason) => {
         if (reason === 'clickaway') {
@@ -41,8 +22,7 @@ function App() {
         setSnack(false);
     };
 
-    // для меню выбора функции найти или загрузить
-    const [menu, setMenu] = useState('find');
+    const [menu, setMenu] = useState('');
     const menuChange = (event, select) => {
         if (!select) {
             return;
@@ -50,52 +30,38 @@ function App() {
         setMenu(select);
     }
 
-    const selectedOption = useMemo(() => {
-        if (menu === 'find') {
-            return (
-                <Find
-                    picture={valuePicture}
-                    setPicture={setValuePicture}
-
-                    modalOpen={modalHasOpen}
-                    modalClose={modalHasClose}
-
-                    getPersons={getPersons}
-
-                    setSnack={setSnack}
-                />
-            );
-        }
-
-        return <Upload picture={valuePicture} setPicture={setValuePicture} setSnack={setSnack} />;
-    }, [menu, valuePicture])
+    // без этого use effect, при перезагрузке страницы, мы бы находились по адрессу /upload
+    // но при этом в меню отображалась кнопка найти, а не загрузить
+    useEffect(() => {
+        setMenu(window.location.pathname);
+    }, [])
 
 
     return (
-        <Container className='app'>
-            <Box className='main flex-center-column'>
-                <DosyLogo />
+        <AppContext.Provider value={{
+            picture,
+            setPicture,
+            setSnack,
+        }}>
+            <BrowserRouter>
+                <Box className='main flex-center-column'>
+                    <DosyLogo />
+                    <Text text={config.aboutText} />
 
-                <Text text={config.aboutText} />
+                    <Menu
+                        buttons={[['найти', '/find'], ['загрузить', '/upload']]}
+                        value={menu}
+                        change={menuChange}
+                    />
 
-                <Menu
-                    buttons={[
-                        ['найти', 'find'],
-                        ['загрузить', 'upload'],
-                    ]}
-                    value={menu}
-                    change={menuChange}
-                />
+                    <AppRouter />
 
-                {selectedOption}
+                    <Text text={config.liabilityText} />
+                </Box>
 
-                <Text text={config.liabilityText} />
-            </Box>
-
-            <ModalForResults open={modalState} closeHandler={modalHasClose} data={valuePersons} />
-            <Snack open={snack[0]} text={snack[1]} severity={snack[2]} close={snackClose} />
-
-        </Container>
+                <Snack open={snack[0]} text={snack[1]} severity={snack[2]} close={snackClose} />
+            </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
