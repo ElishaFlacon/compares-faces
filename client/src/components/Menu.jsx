3ccdf09df3d4fd20fa28dd902f79ffc5ab760017
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-26 00:41:41 +0500

    add link from react router dom

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ToggleButtonGroup, ToggleButton } from '@mui/material';
 import '../styles/Menu.css';
+import { Link } from 'react-router-dom';
 
 
 function Menu(props) {
-
     return (
         <ToggleButtonGroup
             color="primary"
@@ -14,8 +14,10 @@ function Menu(props) {
         >
             {props.buttons.map((data) => {
                 return (
-                    <ToggleButton className='menu-button' value={data[1]} key={data[1]}>
-                        {data[0]}
+                    <ToggleButton className='menu-button' value={data[1]} key={data[1]} sx={{ padding: 0 }}>
+                        <Link to={data[1]} className='menu-link flex-center-row'>
+                            {data[0]}
+                        </Link>
                     </ToggleButton>
                 );
             })}
