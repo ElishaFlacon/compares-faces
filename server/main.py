2e1114e12887eb1307bd3115e6871aa120476630
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-27 22:46:45 +0500

    update for production

--- server/main.py
+++ server/main.py
@@ -5,89 +5,89 @@ from app.file.file_controller import FileController
 from app.recognizer.recognizer_controller import RecognizerController
 from app.person.person_controller import PersonController
 
+def create_app():
+    app = Flask(__name__)
+    cors = CORS(app)
+    load_dotenv(find_dotenv())
 
-app = Flask(__name__)
-cors = CORS(app)
-load_dotenv(find_dotenv())
-
-fileController = FileController()
-recognizerController = RecognizerController()
-personController = PersonController()
-
-
-@app.route('/api/get/image', methods=['GET'])
-@cross_origin()
-def get_image():
-    file = request.args.get('name')
-    path = f'app/static/{file}'
-    return send_file(path, mimetype='image/png')
+    fileController = FileController()
+    recognizerController = RecognizerController()
+    personController = PersonController()
 
 
-@app.route('/api/post/upload-person', methods=['POST'])
-@cross_origin()
-def upload_person():
-    picture = fileController.upload_picture(request.files)
-
-    if (not picture):
-        return 'error'
-
-    face_detect = recognizerController.detect_face(picture)
-    if (not face_detect.get('detect')):
-        fileController.remove_file(picture)
-        return {'load': False}
-
-    result = personController.new_person(request.form, picture)
-    return result
+    @app.route('/api/get/image', methods=['GET'])
+    @cross_origin()
+    def get_image():
+        file = request.args.get('name')
+        path = f'app/static/{file}'
+        return send_file(path, mimetype='image/png')
 
 
-@app.route('/api/post/compare', methods=['POST'])
-@cross_origin()
-def compare():
-    pictures = fileController.upload_pictures(request.files)
-
-    if (not pictures):
-        return 'error'
-
-    result = recognizerController.comparison_faces(pictures)
-    return result
-
-
-@app.route('/api/post/search', methods=['POST'])
-@cross_origin()
-def search():
-    # if we know filename in fronted, so we can send it as a parameter from frontend
-    picture = request.args.get('picture')
-
-    if (not picture):
+    @app.route('/api/post/upload-person', methods=['POST'])
+    @cross_origin()
+    def upload_person():
         picture = fileController.upload_picture(request.files)
 
-    if (not picture):
-        return 'error'
+        if (not picture):
+            return 'error'
 
-    face_detect = recognizerController.detect_face(picture)
-    if (not face_detect.get('detect')):
+        face_detect = recognizerController.detect_face(picture)
+        if (not face_detect.get('detect')):
+            fileController.remove_file(picture)
+            return {'load': False}
+
+        result = personController.new_person(request.form, picture)
+        return result
+
+
+    @app.route('/api/post/compare', methods=['POST'])
+    @cross_origin()
+    def compare():
+        pictures = fileController.upload_pictures(request.files)
+
+        if (not pictures):
+            return 'error'
+
+        result = recognizerController.comparison_faces(pictures)
+        return result
+
+
+    @app.route('/api/post/search', methods=['POST'])
+    @cross_origin()
+    def search():
+        # if we know filename in fronted, so we can send it as a parameter from frontend
+        picture = request.args.get('picture')
+
+        if (not picture):
+            picture = fileController.upload_picture(request.files)
+
+        if (not picture):
+            return 'error'
+
+        face_detect = recognizerController.detect_face(picture)
+        if (not face_detect.get('detect')):
+            fileController.remove_file(picture)
+            return {'load': False}
+
+        result = personController.find_person(picture)
+
+        # remove because it is not needed
         fileController.remove_file(picture)
-        return {'load': False}
 
-    result = personController.find_person(picture)
-
-    # remove because it is not needed
-    fileController.remove_file(picture)
-
-    return result
+        return result
 
 
-@app.route('/api/post/detect', methods=['POST'])
-@cross_origin()
-def detect():
-    picture = fileController.upload_picture(request.files)
+    @app.route('/api/post/detect', methods=['POST'])
+    @cross_origin()
+    def detect():
+        picture = fileController.upload_picture(request.files)
 
-    if (not picture):
-        return 'error'
+        if (not picture):
+            return 'error'
 
-    result = recognizerController.detect_face(picture)
-    return result
+        result = recognizerController.detect_face(picture)
+        return result
 
 
-if __name__ == '__main__':
-    app.run()
+    # if __name__ == '__main__':
+    #     app.run()
