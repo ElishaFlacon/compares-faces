611f4386ec9c2f885efb40d6416d876a6415acfa
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-05-28 18:38:03 +0500

    update api link

--- client/src/API/UploadService.js
+++ client/src/API/UploadService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default class UploadService {
     static async uploadPerson(data) {
-        const response = await axios.post(`/api/post/upload-person`, data);
+        const response = await axios.post(`https://dosy-backend-app.onrender.com/api/post/upload-person`, data);
         return response.data;
     }
 }
